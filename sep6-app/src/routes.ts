3584a69e64d92380d13635dbe1a058c289b5aefa
Giedrius <266934@via.dk>
2020-05-22 19:15:51 +0200

    Added routes to each section

--- sep6-app/src/routes.ts
+++ sep6-app/src/routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { SectionDummyComponent } from './app/sections/section-dummy/section-dummy.component';
+import { SectionFlightsComponent } from './app/sections/section-flights/section-flights.component';
+import { SectionWeatherComponent } from './app/sections/section-weather/section-weather.component';
+import { SectionManufacturersComponent } from './app/sections/section-manufacturers/section-manufacturers.component';
 
 export const appRoutes: Routes = [
     { path: 'dummy', component: SectionDummyComponent },
+    { path: 'flights', component: SectionFlightsComponent },
+    { path: 'weather', component: SectionWeatherComponent },
+    { path: 'manufacturers', component: SectionManufacturersComponent },
 
     { path: '', redirectTo: '/dummy', pathMatch: 'full'}
 ];
