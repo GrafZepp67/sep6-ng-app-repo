031ba6085f337f51d113dd20dbac24670b123cf6
Giedrius <266934@via.dk>
2020-05-22 21:27:11 +0200

    Added link to new function

--- sep6-app/src/app/services/dummy-data.service.ts
+++ sep6-app/src/app/services/dummy-data.service.ts
@@ -10,6 +10,6 @@ export class DummyDataService {
 
     getDummyData()
     {
-        return this._http.get('https://manufacturersapi-4z5zwd6dlq-ew.a.run.app/getAllStudents?key=AIzaSyCeSR2ZQNvqJX9kWdUjOLheNVZ-Z_S5i-A');
+        return this._http.get('https://us-central1-sep6s20.cloudfunctions.net/getDummyData');
     }
 }
