265034ad5086fc992a58276285cbadbe22799877
Giedrius <266934@via.dk>
2020-06-02 10:57:26 +0200

    Fixing redirection bug

--- sep6-app/server.js
+++ sep6-app/server.js
@@ -2,6 +2,6 @@ var express = require('express');
 var app = express();
 app.use(express.static('dist/sep6-app'));
 app.all('/*', function(req, res, next) {
-    res.redirect('/dashboard');
+    res.redirect('/');
 });
 app.listen(8080)
